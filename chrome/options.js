7b90482eb7ea008469a78741ad8f371157b94946
Jordi Subirà Nieto <jordi.subira.nieto@gmail.com>
2022-01-24 11:20:08 +0100

    add toggled ISD at sync storage + eventHandler

--- chrome/options.js
+++ chrome/options.js
@@ -4,6 +4,21 @@
 // Copyright 2021 ETH, Ovgu
 'use strict';
 
+const placeholderToggleID = "toggleISD-"
+
+window.onload = function () {
+    getStorageValue('isd_whitelist').then((isdSet) => {
+        displayToggleISD(isdSet);
+      });
+}
+
+function displayToggleISD(isdSet){
+    for (const id of isdSet){
+        var isdToggle = document.getElementById(placeholderToggleID + id);
+        isdToggle.checked = true;
+    }
+}
+
 function registerToggleISDHandler() {
 
     const idsToggles = document.getElementsByClassName("isd-entry");
@@ -19,36 +34,33 @@ registerToggleISDHandler();
 function toggleISD(checked_id){
     var isdToggle = document.getElementById(checked_id);
     isdToggle.checked = !isdToggle.checked;
+    var id = checked_id.split("toggleID-")[1];
+    applyWhitelist(id, isdToggle.checked);
 }
 
-function applyWhitelist(){
-    var isds = document.getElementsByClassName("isd-entry");
-    let isdWhiteList = new Array()
-    for(var i = 0; i < isds.length; i++) {
-       if (isds[i].getAttribute("checked")){
-           // push isd number
-       }
+// function applyWhitelist(){
+//     var isds = document.getElementsByClassName("isd-entry");
+//     let isdWhiteList = new Array()
+//     for(var i = 0; i < isds.length; i++) {
+//        if (isds[i].getAttribute("checked")){
+//            // push isd number
+//        }
+//     }
+//     saveGeofence(isdWhiteList);
+// }
+
+
+async function applyWhitelist(isd, checked){
+    const isdList = await getStorageValue('isd_whitelist');
+    const isdSet = await toSet(isdList);
+    if (checked) {
+        isdSet.add(isd);
+        console.log('Added isd to list: ' + isd);
+    } else {
+        isdSet.delete(isd);
+        console.log('Delete isd to list: ' + isd);
     }
-    geofence(isdWhiteList);
+    const isdSet_1 = isdSet;
+    await saveStorageValue('isd_whitelist', [...isdSet_1]);
+    console.log([...isdSet_1]);
 }
-
-function geofence(isdList){
-    // Transform list to string
-    let stringList = ""
-    for (const isd of isdList){
-      stringList += isd + ",";
-    }
-
-    var req = new XMLHttpRequest();
-    req.open("PUT", "http://localhost:8888/setISDPolicy", true);
-    req.setRequestHeader('Content-type','application/raw; charset=utf-8');
-    req.onreadystatechange = function() {
-        if (req.readyState == 4) {
-          if (req.status == 200) {
-            const resp = req.responseText;
-            console.log("Response from SKIP: " + resp);
-            }
-        }
-    };
-    req.send(stringList);
-  }
