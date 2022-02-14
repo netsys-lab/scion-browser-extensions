9e3a3aebd23259c6f1cc8f6cac4387bf462fa90a
Marten Gartner <martenwallewein@web.de>
2022-02-14 11:24:23 +0100

    Fix ISD toggling to skip

--- chrome/options.js
+++ chrome/options.js
@@ -10,17 +10,19 @@ window.onload = function () {
     getStorageValue('isd_whitelist').then((isdSet) => {
         displayToggleISD(isdSet);
       });
+      registerToggleISDHandler();
 }
 
 function displayToggleISD(isdSet){
     for (const id of isdSet){
         var isdToggle = document.getElementById(placeholderToggleID + id);
-        isdToggle.checked = true;
+        if(isdToggle) {
+            isdToggle.checked = true;    
+        }
     }
 }
 
 function registerToggleISDHandler() {
-
     const idsToggles = document.getElementsByClassName("isd-entry");
     for (let i = 0; i < idsToggles.length; i++) {
         const parentDiv = idsToggles[i].parentElement;
@@ -29,12 +31,12 @@ function registerToggleISDHandler() {
         }
     }
 };
-registerToggleISDHandler();
+
 
 function toggleISD(checked_id){
     var isdToggle = document.getElementById(checked_id);
     isdToggle.checked = !isdToggle.checked;
-    var id = checked_id.split("toggleID-")[1];
+    var id = checked_id.split("toggleISD-")[1];
     applyWhitelist(id, isdToggle.checked);
 }
 
@@ -50,9 +52,30 @@ function toggleISD(checked_id){
 // }
 
 
+function geofence(isdList){
+    // Transform list to string
+    let stringList = ""
+    for (const isd of isdList){
+      stringList += isd + ",";
+    }
+
+    var req = new XMLHttpRequest();
+    req.open("PUT", "http://localhost:8888/setISDPolicy", true);
+    req.setRequestHeader('Content-type','application/raw; charset=utf-8');
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+          if (req.status == 200) {
+            const resp = req.responseText;
+            console.log("Response from SKIP: " + resp);
+            }
+        }
+    };
+    req.send(stringList);
+  }
+
 async function applyWhitelist(isd, checked){
     const isdList = await getStorageValue('isd_whitelist');
-    const isdSet = await toSet(isdList);
+    const isdSet = await toSet(removeEmptyEntries(isdList));
     if (checked) {
         isdSet.add(isd);
         console.log('Added isd to list: ' + isd);
@@ -63,4 +86,9 @@ async function applyWhitelist(isd, checked){
     const isdSet_1 = isdSet;
     await saveStorageValue('isd_whitelist', [...isdSet_1]);
     console.log([...isdSet_1]);
+    geofence([...isdSet_1]);        
+}
+
+function removeEmptyEntries(list) {
+    return list.filter(l => !!l);
 }
